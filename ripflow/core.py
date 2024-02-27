66260d055934a132a88bd50bbf821a53bad3db70
Soeren Jalas <soeren.jalas@desy.de>
2024-02-27 08:35:36 +0100

    refactoring

--- ripflow/core.py
+++ ripflow/core.py
@@ -1,11 +1,12 @@
 import logging
-from typing import Optional, List
+from typing import List
 from .connectors.source import SourceConnector
 from .connectors.sink import SinkConnector
 from .analyzers import BaseAnalyzer
-from multiprocessing import Process
+from .supervisor import Supervisor, RestartPolicy
+import sys
 import zmq
-import time
+from .utils import Child, ZMQFactory, CommsFactory
 
 
 class MiddleLayerAnalyzer(object):
@@ -45,6 +46,9 @@ class MiddleLayerAnalyzer(object):
         # Create file handler and set level to log_level
         file_handler = logging.FileHandler(log_file_path)
         file_handler.setLevel(log_level)
+        # Create stream handler for output to stdout
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(log_level)
         # Create formatter
         formatter = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -68,15 +72,72 @@ class MiddleLayerAnalyzer(object):
         self.analyzer.logger = self.logger
 
         # Parameters of comm layer
-        self.worker_socket_address = "ipc://workers"
+        self.source_socket_address = "ipc://source"
         self.sender_socket_address = "ipc://sender"
+
+        self.comms_factory = ZMQFactory()
+        self.producer_comms_config = {
+            "socket_type": zmq.PUSH,
+            "bind_address": self.source_socket_address,
+        }
+        self.worker_input_comms_config = {
+            "socket_type": zmq.PULL,
+            "connect_address": self.source_socket_address,
+        }
+        self.worker_output_comms_config = {
+            "socket_type": zmq.PUSH,
+            "connect_address": self.sender_socket_address,
+        }
+        self.sender_comms_config = {
+            "socket_type": zmq.PULL,
+            "bind_address": self.sender_socket_address,
+        }
 
         # Process registries
         self.n_workers = n_workers
         self.n_senders = analyzer.n_outputs
-        self.workers = [Worker(self, worker_id=i) for i in range(self.n_workers)]
-        self.senders = [Sender(self, idx=i) for i in range(self.n_senders)]
-        self.producer = Producer(self)
+        self.workers = [
+            Worker(
+                logger=self.logger,
+                comms_factory=self.comms_factory,
+                input_comms_config=self.worker_input_comms_config,
+                output_comms_config=self.worker_output_comms_config,
+                analyzer=self.analyzer,
+                sink_connector=self.sink_connector,
+                n_senders=self.n_senders,
+                worker_id=i,
+            )
+            for i in range(self.n_workers)
+        ]
+        self.senders = [
+            Sender(
+                logger=self.logger,
+                comms_factory=self.comms_factory,
+                comms_config=self.sender_comms_config,
+                sink_connector=self.sink_connector,
+                idx=i,
+            )
+            for i in range(self.n_senders)
+        ]
+        self.producer = Producer(
+            logger=self.logger,
+            comms_factory=self.comms_factory,
+            comms_config=self.producer_comms_config,
+            source_connector=self.source_connector,
+        )
+
+        # Supervisor definition
+        self.restart_policy = RestartPolicy(
+            n_restart=3, restart_delay=5, reset_window=60
+        )  # Restart policy for processes
+        self.supervisor = Supervisor(
+            logger=self.logger
+        )  # Supervisor for managing processes
+        self.supervisor.add_process(self.producer, self.restart_policy)
+        for worker in self.workers:
+            self.supervisor.add_process(worker, self.restart_policy)
+        for sender in self.senders:
+            self.supervisor.add_process(sender, self.restart_policy)
 
     def event_loop(self, background=False):
         """Start main event loop
@@ -88,18 +149,17 @@ class MiddleLayerAnalyzer(object):
             background. If true will spawn subprocess and continue,
             by default False.
         """
-        for worker in self.workers:
-            worker.launch()
-        for sender in self.senders:
-            sender.launch()
-        time.sleep(1)
-        if not background:
-            self.producer._producer_routine()
-        else:
-            self.producer.launch()
+        self.supervisor.start_all_processes()
+        # self.producer.launch()
+        # time.sleep(1)
+        # for sender in self.senders:
+        #     sender.launch()
+        # time.sleep(1)
+        # for worker in self.workers:
+        #     worker.launch()
 
 
-class Producer(object):
+class Producer(Child):
     """_summary_
 
     Parameters
@@ -108,126 +168,140 @@ class Producer(object):
         Server object to connect to
     """
 
-    def __init__(self, server: MiddleLayerAnalyzer) -> None:
+    def __init__(
+        self,
+        logger: logging.Logger,
+        comms_factory: CommsFactory,
+        comms_config: dict,
+        source_connector: SourceConnector,
+    ) -> None:
         """Construct producer object"""
-        self.logger: logging.Logger = server.logger
-        self.source_connector: SourceConnector = server.source_connector
-        self.worker_socket_address: str = server.worker_socket_address
-        self.process: Optional[Process] = None
+        super().__init__(logger, comms_factory)
+        self.source_connector = source_connector
+        self.comms_config = comms_config
 
-    def launch(self) -> None:
-        self.process = Process(target=self._producer_routine, daemon=True)
-        self.process.start()
-        self.logger.info("Producer launched")
-
-    def _producer_routine(self):
+    def main_routine(self):
         """Listen for incoming events."""
-        self.context = zmq.Context()
-        self.running = True
+        self.context = self.comms_factory.create_context()
         self.source_connector.connect()
         self._connect_producer()
-        while self.running:
-            data = self.source_connector.get_data()
-            self.input_socket.send_pyobj(data)
+        while True:
+            try:
+                data = self.source_connector.get_data()
+                self.input_socket.send_pyobj(data)
+            except Exception as e:
+                self.logger.error(f"Error in producer main_routine: {e}")
+                break
 
     def _connect_producer(self):
-        self.input_socket = self.context.socket(zmq.PUSH)
-        self.input_socket.bind(self.worker_socket_address)
+        self.input_socket = self.comms_factory.create_socket(
+            self.context, **self.comms_config
+        )
 
 
-class Worker(object):
-    """
-    A worker object that connects to a server and processes data using an analyzer.
+class Worker(Child):
+    def __init__(
+        self,
+        logger: logging.Logger,
+        comms_factory: CommsFactory,
+        input_comms_config: dict,
+        output_comms_config: dict,
+        analyzer: BaseAnalyzer,
+        sink_connector: SinkConnector,
+        n_senders: int,
+        worker_id: int = 0,
+    ) -> None:
+        """
+        Initialize the Worker object.
 
-    Parameters
-    ----------
-    server : MiddleLayerAnalyzer
-        Server object to connect to
-    worker_id : int, optional
-        Worker id, by default 0
-    """
-
-    def __init__(self, server: MiddleLayerAnalyzer, worker_id: int = 0) -> None:
-        """Construct worker object"""
-        self.logger = server.logger
+        Args:
+            logger (logging.Logger): The logger object for logging messages.
+            comms_factory (CommsFactory): The factory object for creating communication objects.
+            input_comms_config (dict): The configuration for input communication.
+            output_comms_config (dict): The configuration for output communication.
+            analyzer (BaseAnalyzer): The analyzer object for analyzing data.
+            sink_connector (SinkConnector): The sink connector object for connecting to sinks.
+            n_senders (int): The number of senders.
+            worker_id (int, optional): The ID of the worker. Defaults to 0.
+        """
+        super().__init__(logger, comms_factory)
+        self.input_comms_config = input_comms_config
+        self.output_comms_config = output_comms_config
+        self.analyzer = analyzer
+        self.sink_connector = sink_connector
+        self.n_senders = n_senders
         self.worker_id = worker_id
-        self.analyzer = server.analyzer
-        self.sink_connector = server.sink_connector
-        self.sender_socket_address = server.sender_socket_address
-        self.worker_socket_address = server.worker_socket_address
-        self.n_senders = server.n_senders
-        self.sender_sockets: List[zmq.Socket] = list()
-        self.process: Optional[Process] = None
+        self.output_sockets: List[zmq.Socket] = list()
 
-    def launch(self) -> None:
-        """
-        Launch worker process.
-        """
-        self.process = Process(target=self._worker_routine, args=(self.worker_id,))
-        self.process.daemon = True
-        self.process.start()
-        self.logger.info(f"Worker {self.worker_id} launched")
-
-    def _worker_routine(self, worker_id: int = 0):
-        """
-        Worker routine that receives and processes data.
-
-        Parameters
-        ----------
-        worker_id : int, optional
-            Worker id, by default 0
-        """
-        self.context = zmq.Context()
+    def main_routine(self):
+        self.context = self.comms_factory.create_context()
         self._connect_worker()
-        self.logger.info(f"Worker {worker_id} launched")
+        self.logger.info(f"Worker {self.worker_id} launched")
         while True:
-            data = self.worker_data_socket.recv_pyobj()
+            data = self.input_socket.recv_pyobj()
             data = self.analyzer.run(data)
             for idx in range(self.n_senders):
                 prop = data[idx]
                 msg = self.sink_connector.serializer.serialize(prop)
-                self.sender_sockets[idx].send(msg)
+                self.output_sockets[idx].send(msg)
 
-    def _connect_worker(self) -> None:
-        """
-        Connect worker IO sockets.
-        """
-        self.worker_data_socket = self.context.socket(zmq.PULL)
-        self.worker_data_socket.connect(self.worker_socket_address)
+    def _connect_worker(self):
+        self.input_socket = self.comms_factory.create_socket(
+            self.context, **self.input_comms_config
+        )
+        base_config = self.output_comms_config.copy()
         for idx in range(self.n_senders):
-            socket = self.context.socket(zmq.PUSH)
-            socket.connect(self.sender_socket_address + f"_{idx:d}")
-            self.sender_sockets.append(socket)
+            # Modify the specific configuration for each sender
+            config = base_config.copy()
+            address_key = (
+                "bind_address" if "bind_address" in config else "connect_address"
+            )
+            config[address_key] = config[address_key] + f"_{idx}"
+            socket = self.comms_factory.create_socket(self.context, **config)
+            self.output_sockets.append(socket)
 
 
-class Sender(object):
+class Sender(Child):
     """
-    Sender object for processing and sending data.
+    Represents a sender in the ripflow system.
 
     Parameters
     ----------
-    server : MiddleLayerAnalyzer
-        Server object to connect to.
+    logger : logging.Logger
+        The logger object for logging messages.
+    comms_factory : CommsFactory
+        The factory object for creating communication objects.
+    comms_config : dict
+        The configuration for the communication objects.
     idx : int
-        Sender id.
+        The sender id.
+    sink_connector : SinkConnector
+        The sink connector object for sending messages to the sink.
+
+    Attributes
+    ----------
+    idx : int
+        The sender id.
+    comms_config : dict
+        The configuration for the communication objects.
+    sink_connector : SinkConnector
+        The sink connector object for sending messages to the sink.
     """
 
-    def __init__(self, server: MiddleLayerAnalyzer, idx: int) -> None:
-        """Construct sender object"""
-        self.logger = server.logger
+    def __init__(
+        self,
+        logger: logging.Logger,
+        comms_factory: CommsFactory,
+        comms_config: dict,
+        idx: int,
+        sink_connector: SinkConnector,
+    ) -> None:
+        super().__init__(logger, comms_factory)
         self.idx = idx
-        self.sink_connector = server.sink_connector
-        self.sender_socket_address = server.sender_socket_address
-        self.process: Optional[Process] = None
+        self.comms_config = comms_config
+        self.sink_connector = sink_connector
 
-    def launch(self) -> None:
-        """Launch the sender process."""
-        self.process = Process(target=self._sender_routine, args=(self.idx,))
-        self.process.daemon = True
-        self.process.start()
-        self.logger.info(f"Sender {self.idx} launched")
-
-    def _sender_routine(self, idx: int) -> None:
+    def main_routine(self) -> None:
         """
         The sender routine.
 
@@ -236,15 +310,17 @@ class Sender(object):
         idx : int
             The sender id.
         """
-        self.context = zmq.Context()
-        self._connect_sender(idx)
-        self.sink_connector.connect_subprocess(idx)
-        self.logger.info(f"Sender {idx} launched")
+        self.context = self.comms_factory.create_context()
+        self._connect_sender()
+        self.sink_connector.connect_subprocess(self.idx)
+        self.logger.info(f"Sender {self.idx} launched")
         while True:
-            msg = self.sender_data_socket.recv()
+            msg = self.input_socket.recv()
             self.sink_connector.send(msg)
 
-    def _connect_sender(self, idx: int):
+    def _connect_sender(self):
         """Connect sender to processed data stream"""
-        self.sender_data_socket = self.context.socket(zmq.PULL)
-        self.sender_data_socket.bind(self.sender_socket_address + f"_{idx:d}")
+        config = self.comms_config.copy()
+        address_key = "bind_address" if "bind_address" in config else "connect_address"
+        config[address_key] = config[address_key] + f"_{self.idx}"
+        self.input_socket = self.comms_factory.create_socket(self.context, **config)
---
+++ ripflow/supervisor.py
@@ -0,0 +1,124 @@
+from .utils import Child
+import time
+from threading import Thread
+from datetime import datetime, timedelta
+
+
+class RestartPolicy:
+    def __init__(self, n_restart: int, restart_delay: int, reset_window: int):
+        self._n_restart = n_restart
+        self._restart_delay = restart_delay
+        self._reset_window = reset_window
+
+    @property
+    def n_restart(self):
+        return self._n_restart
+
+    @property
+    def restart_delay(self):
+        return self._restart_delay
+
+    @property
+    def reset_window(self):
+        return self._reset_window
+
+
+class Supervisor(object):
+    def __init__(self, logger=None):
+        self._processes = {}  # Stores Child processes with their policies and metadata
+        self.logger = logger
+
+    def add_process(self, process: Child, policy: RestartPolicy):
+        """
+        Adds a process to the supervisor with a specified restart policy.
+        """
+        self._processes[process] = {
+            "policy": policy,
+            "restart_count": 0,
+            "last_restart": None,
+            "reset_timer": datetime.now(),
+            "thread": None,  # Placeholder for the monitoring thread
+        }
+
+    def start_all_processes(self):
+        """
+        Starts all managed child processes.
+        """
+        for process in self._processes.keys():
+            self.start_process(process)
+
+    def start_process(self, process: Child):
+        """
+        Starts a single child process and initializes its monitoring thread.
+        """
+        # Ensure the process isn't already running
+        if not process.is_alive():  # type: ignore
+            process.launch()  # type: ignore
+            self.logger.info(f"Supervisor: Process {process} started.")
+        else:
+            self.logger(f"Supervisor: Process {process} is already running.")
+
+    def _reset_restart_count(self, process: Child):
+        """
+        Resets the restart count for a process based on the reset_window.
+        """
+        process_info = self._processes[process]
+        if datetime.now() >= process_info["reset_timer"]:
+            process_info["restart_count"] = 0
+            process_info["reset_timer"] = datetime.now() + timedelta(
+                seconds=process_info["policy"].reset_window
+            )
+
+    def restart_process(self, process: Child):
+        """
+        Attempts to restart a process according to its restart policy.
+        """
+        process_info = self._processes[process]
+        policy = process_info["policy"]
+
+        self._reset_restart_count(process)
+
+        if process_info["restart_count"] < policy.n_restart:
+            time.sleep(policy.restart_delay)  # Wait before restarting
+            process.launch()  # type: ignore
+            process_info["restart_count"] += 1
+            process_info["last_restart"] = datetime.now()
+            print(
+                f"Process {process} restarted. Count: {process_info['restart_count']}"
+            )
+        else:
+            print(f"Process {process} reached maximum restart limit.")
+
+    def stop_process(self, process: Child):
+        """
+        Stops a given process.
+        """
+        process.stop()  # type: ignore
+        if process in self._processes:
+            process_info = self._processes[process]
+            if process_info["thread"]:
+                process_info["thread"].join()  # Wait for monitoring thread to finish
+            del self._processes[process]
+
+    def _monitor_process(self, process: Child):
+        """
+        Monitors a process and restarts it if it stops unexpectedly.
+        """
+        while True:
+            if not process.is_alive():  # type: ignore
+                self.logger.info(f"Supervisor: Process {process} stopped unexpectedly.")
+                self.restart_process(process)
+            time.sleep(1)  # Polling interval
+
+    def monitor_processes(self):
+        """
+        Starts monitoring threads for all managed processes.
+        """
+        for process in self._processes.keys():
+            if not self._processes[process]["thread"]:
+                thread = Thread(target=self._monitor_process, args=(process,))
+                thread.start()
+                self._processes[process]["thread"] = thread
+                self.logger.info(
+                    f"Supervisor: Monitoring thread for process {process} started."
+                )
